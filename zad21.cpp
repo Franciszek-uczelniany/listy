2f743c4b75133a708f9806cfbebebf62d8905217
Franciszek222 <fm2337524@gmail.com>
2025-01-07 09:46:11 +0100

    Add files via upload

--- zad21.cpp
+++ zad21.cpp
@@ -1,13 +1,17 @@
-﻿
+﻿//Do zaimplementowania: metoda shella na bazie sortowania babelkowego i quick sort
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#define NUM 100
+
+#include <algorithm>
+#define NUM 50
 
 void bubble(int* tablica, unsigned int size);
 void wstawienie(int* tab, unsigned int size);
+void qsort(int* A, int p, int r);
+int partition(int* A, int p, int r);
 
 int main()
 {
@@ -26,16 +30,97 @@ int main()
     // drukujemy wyniki.....
 
     // free(tablica);
+    int size = NUM;
 
+    qsort(tablica, 0, --size);
+
+
+
+    return 0;
+    /*
     bubble(tablica, NUM);
     wstawienie(tablica, NUM);
+    */
+    }
+
+
+
+
+//todo: zrobic funkcje partition w taki sposob aby robiła trzy partycje
+
+// funkcja dzieli tablicę A na dwie części A[p,i] <= x <=A[j,r]
+int partition(int* A , int p, int r)
+{
+    int _r = rand() % (r - p + 1) + p;
+
+    int x, i, j;
+    int y;
+
+    x = A[p]; A[p] = A[_r]; A[_r] = x;
+
+    x = A[p];  // wybór pivotu, w tym przypadku efektywnie będzie to A[_r]
+    i = p - 1; j = r + 1;
+
+    do {
+        do { j--; } while (A[j] > x);
+        do { i++; } while (A[i] < x);
+        if (i < j) {
+            y = A[i];
+            A[i] = A[j];
+            A[j] = y;
+        } 
+
+    } while (i < j);
+
+    if (i > j) printf("\n UWAGA: i>j");
+    else if (i == j) {
+        printf("\n UWAGA: i == j");
+        maxi++;
+    }
+
+    int mini = std::min(i, j);
+    int maxi = std::max(i, j);
+
+
+
+    printf("\n\n pivot: %d", x);
+    printf("\n A[p,i]: ");
+
+    for (unsigned int h = p; h <= mini; h++)
+        printf("\n %d", A[h]);
+
+    printf("\n A[j,r]: ");
+    for (unsigned int h = maxi; h <= r; h++)
+            printf("\n %d", A[h]);
+
+
+    printf("\n");
+    return j;
+
 }
 
+void part_verify(int* A, int p, int i, int j, int r) {
 
-//Do zaimplementowania: metoda shella na bazie sortowania babelkowego i quick sort
+    //for (unsigned g = 0; );
 
-//TODO: ZADANIE DOMOWE
-int partition();
+    // byc moze wystarczy znalezc najwiekszy element pierwszej partycji
+    // i sprawdzic czy istnieje element mniejszy od tego najwiekszego w drugiej
+    // partycji.
+
+}
+
+void qsort(int* A, int p, int r)
+{
+    int buff;
+    if (p < r)
+    {
+        buff = partition(A, p, r);
+        qsort(A, p, buff);
+        qsort(A, buff + 1, r);
+
+    }
+
+}
 
 void wstawienie(int* tab, unsigned int size) {
 
