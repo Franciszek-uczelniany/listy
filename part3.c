9d3d0ef1acdc36deb09072e395995df32a05d199
Your Name <you@example.com>
2025-01-16 13:09:17 +0100

    notatki z poprzednich algo

---
+++ part3.c
@@ -0,0 +1,49 @@
+Wynik part3(int A[], int p, int r){
+
+
+int x,y,z,j, i, i2
+x=A[r]
+i=p
+i2=p
+for(j=p, j<=r-1, j++){
+if A[j] < x){
+y=A[i], A[i]=A[j], A[j]=y;
+i++
+if(i>i2) i2++
+
+} 
+
+ if (A[j] ==x){
+y=A[i2]; A[i2] = A[r]; A[r]=y;
+i2++
+}}
+y=A[i2]; A[i2]=A[r]; A[r]=y;
+wynik Wynik;
+wynik a =i;
+wynik b= i2
+return wynik;
+
+
+algorytm 5 piatek
+przy czym koszt sortowania tych 5 jest duzo suzo mniwjszy.
+nie rozumiem jaki wniosek??????
+i drugi shora
+
+
+dyscyplina sportu jaka jest mknimalna liczba if w sort przez porownywanie
+5 elementow tablicy jak najszybciej posortowac bez ifow. lepiej samwemu poukladac ify
+zaczac od 3 elementow
+
+mnozdnie macierzy z matsmtyczne punktu widzwnia jest przemienne, nie rozumiem tego
+
+c++ (na razie tylko te szablony) i ACM zronic te projekty przeciez
+wifi fortinet komtroler
+
+
+emacs
+
+na przyszly tydzien
+144/228 strony ogarmac sobiw i 151 cykl eylera i przeszukiwanie grafu wstecz
+
+30.01 to ostatnie kolokwium
+projket z mqtlaba zaczac robic i za tydz nie ma zajec
