47e7b10a8dedb9e9b49597b0bc784a8711325112
Your Name <you@example.com>
2025-01-28 05:53:24 +0100

    update

--- zad21.cpp
+++ zad21.cpp
@@ -1,312 +1,187 @@
-﻿//Do zaimplementowania: metoda shella na bazie sortowania babelkowego i quick sort
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <iostream>
+#include <limits.h>
 
-#include <algorithm>
-#define NUM 100
-#define VERBOSE 1
-#define N 10000
-#define M 1000
+// Definicja stałych
+#define N 10000 // Rozmiar tablicy
+#define M 1000  // Liczba powtórzeń
 
-
-
+// Struktura do przechowywania statystyk
 struct stats {
-    long wstawienia = 0;
-    long porownania = 0;
-
+    long porownania;
+    long podstawienia;
 };
 
-struct stats bubble(int* tablica, unsigned int size);
-struct stats wstawienie(int* tab, unsigned int size);
-void qsort(int* A, int p, int r);
-int partition(int* A, int p, int r);
-struct stats shell(int* A);
+// Funkcje sortujące
+struct stats bubble_sort(int* tablica, int size);
+struct stats insertion_sort(int* tablica, int size);
+struct stats shell_sort(int* tablica, int size);
+struct stats quick_sort(int* tablica, int size);
+void quick_sort_recursive(int* A, int low, int high, struct stats* stat);
+int partition(int* A, int low, int high, struct stats* stat);
 
+// Funkcja pomocnicza do kopiowania tablicy
+void copy_array(int* source, int* dest, int size);
 
-// Powinienem zrobic qsort w formie iteracyjnej
+// Funkcja do agregacji statystyk
+struct stats aggregate_stats(struct stats* results, int count);
 
-unsigned int minimum(int* A, int size) {
-
-    int ret = A[0];
-
-    for (int i = 0; i < size; i++) if (A[i] < ret) ret = A[i];
-
-    return ret;
-}
-
-unsigned int maximum(int* A, int size) {
-
-    int ret = A[0];
-
-    for (int i = 0; i < size; i++) if (A[i] < ret) ret = A[i];
-
-    return ret;
-}
-
-int main()
-{
+// Funkcja główna
+int main() {
     srand(time(NULL));
-    int* tablica = (int*) malloc(sizeof(int) * NUM);
+    int* original_array = (int*)malloc(sizeof(int) * N);
+    int* working_array = (int*)malloc(sizeof(int) * N);
 
-    // powtarzamy tą czynnosc 1000 razy (M)
-    struct stats* wyniki_bu = new struct stats[M];
-    struct stats* wyniki_ws = new struct stats[M];
-    struct stats* wyniki_qs = new struct stats[M];
-    struct stats* wyniki_sh = new struct stats[M];
+    struct stats bubble_stats[M], insertion_stats[M], shell_stats[M], quick_stats[M];
 
-
-
-
+    // Główna pętla testowa
     for (int i = 0; i < M; i++) {
-    
-        for (int i = 0; i < NUM; i++)
-            tablica[i] = rand() % N; // ustalamy, że liczby są z zakresu 1 do 10 000
-        
-        
-        wyniki_bu[i] = bubble(tablica, NUM);
-        wyniki_ws[i] = wstawienie(tablica, NUM);
-      //  wyniki_qs[i] = qsort(tablica, NUM);
-        wyniki_sh[i] = shell(tablica);
-
-    }
-
-    // Drukujemy wyniki
-    printf("\n Typ sortowania \t MIN wstawien \t MAX wstawien \t srednia wstawien \t MIN porownan \t MAX porownan \t srednia porownan");
-//    printf("\n sortowanie babelkowe \t %ld \t %ld", );
-  
-
-
-
-
-    // Dla każdej z
-   //  wymienionych metod program powinien wyświetlić w tabeli następujące dane : maksymalna
-     //   i minimalna w serii M prób liczba wykonanych podstawień i porównań sortowanych kluczy
-     //   oraz średnia liczba podstawień i porównań.
-
-    // free(tablica);
-    int size = NUM;
-
-    qsort(tablica, 0, --size);
-
-
-
-
-    
-    bubble(tablica, NUM);
-    wstawienie(tablica, NUM);
-    
-
-printf("\n");
-
-
-return 0;
-    }
-
-struct stats shell(int* A) {
-
-    struct stats ret;
-
-    int h, i, j, x;
-    for (h = 1; h < NUM; h = 3 * h + 1);
-    h /= 9;
-    if (!h) h++;
-        
-  
-
-    while (h)
-    {
-        for (j = NUM - h - 1; j >= 0; j--)
-        {
-            x = A[j];
-            i = j + h;
-            while ((i < NUM) && (x > A[i]))
-            {
-                ret.porownania++; // tutaj chodzi o porownanie wyzej w warunku petli while
-
-                A[i - h] = A[i];
-                ret.wstawienia++;
-                i += h;
-            }
-            A[i - h] = x;
-            ret.wstawienia++;
+        // Generowanie losowej tablicy
+        for (int j = 0; j < N; j++) {
+            original_array[j] = rand() % 10000; // Losowe liczby 0-9999
         }
-        h /= 3;
+
+        // Sortowanie i zbieranie statystyk dla każdej metody
+        copy_array(original_array, working_array, N);
+        bubble_stats[i] = bubble_sort(working_array, N);
+
+        copy_array(original_array, working_array, N);
+        insertion_stats[i] = insertion_sort(working_array, N);
+
+        copy_array(original_array, working_array, N);
+        shell_stats[i] = shell_sort(working_array, N);
+
+        copy_array(original_array, working_array, N);
+        quick_stats[i] = quick_sort(working_array, N);
     }
 
-#ifdef VERBOSE
-    printf("\n posortowane: ");
-    for (int i = 0; i < NUM; i++)
-        printf("\n %d", A[i]);
-#endif
+    // Wyliczanie i wyświetlanie wyników
+    printf("\nAlgorytm\tMIN Podstawień\tMAX Podstawień\tŚR Podstawień\tMIN Porównań\tMAX Porównań\tŚR Porównań\n");
 
+    struct stats bubble_summary = aggregate_stats(bubble_stats, M);
+    printf("Bąbelkowe\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\n",
+           bubble_summary.podstawienia, bubble_summary.podstawienia, bubble_summary.podstawienia,
+           bubble_summary.porownania, bubble_summary.porownania, bubble_summary.porownania);
 
-    return ret;
+    struct stats insertion_summary = aggregate_stats(insertion_stats, M);
+    printf("Wstawianie\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\n",
+           insertion_summary.podstawienia, insertion_summary.podstawienia, insertion_summary.podstawienia,
+           insertion_summary.porownania, insertion_summary.porownania, insertion_summary.porownania);
+
+    struct stats shell_summary = aggregate_stats(shell_stats, M);
+    printf("Shell\t\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\n",
+           shell_summary.podstawienia, shell_summary.podstawienia, shell_summary.podstawienia,
+           shell_summary.porownania, shell_summary.porownania, shell_summary.porownania);
+
+    struct stats quick_summary = aggregate_stats(quick_stats, M);
+    printf("QuickSort\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\t\t%ld\n",
+           quick_summary.podstawienia, quick_summary.podstawienia, quick_summary.podstawienia,
+           quick_summary.porownania, quick_summary.porownania, quick_summary.porownania);
+
+    // Zwolnienie pamięci
+    free(original_array);
+    free(working_array);
+
+    return 0;
 }
 
-
-//todo: zrobic funkcje partition w taki sposob aby robiła trzy partycje
-
-// funkcja dzieli tablicę A na dwie części A[p,i] <= x <=A[j,r]
-int partition(int* A , int p, int r)
-{
-    int _r = rand() % (r - p + 1) + p;
-
-    int x, i, j;
-    int y;
-
-    x = A[p]; A[p] = A[_r]; A[_r] = x;
-
-    x = A[p];  // wybór pivotu, w tym przypadku efektywnie będzie to A[_r]
-    i = p - 1; j = r + 1;
-
-    do {
-        do { j--; } while (A[j] > x);
-        do { i++; } while (A[i] < x);
-        if (i < j) {
-            y = A[i];
-            A[i] = A[j];
-            A[j] = y;
-        } 
-
-    } while (i < j);
-
-    if (i > j) printf("\n UWAGA: i>j");
-    else if (i == j) {
-        printf("\n UWAGA: i == j");
-        // maxi++ nie moze tu byc poniewaz maxi nie jest zadeklarowane
+// Implementacja funkcji sortujących
+struct stats bubble_sort(int* tablica, int size) {
+    struct stats stat = {0, 0};
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            stat.porownania++;
+            if (tablica[j] > tablica[j + 1]) {
+                int temp = tablica[j];
+                tablica[j] = tablica[j + 1];
+                tablica[j + 1] = temp;
+                stat.podstawienia++;
+            }
+        }
     }
-
-    int mini = std::min(i, j);
-    int maxi = std::max(i, j);
-    //if(mini == maxi) maxi++;
-
-#ifdef VERBOSE_
-    printf("\n\n pivot: %d", x);
-    printf("\n A[p,i]: ");
-
-    for (unsigned int h = p; h <= mini; h++)
-        printf("\n %d", A[h]);
-
-    printf("\n A[j,r]: ");
-    for (unsigned int h = maxi; h <= r; h++)
-            printf("\n %d", A[h]);
-
-
-    printf("\n");
-#endif
-
- return j;
-      //return maxi; // tymczasowe rozwiązanie.
+    return stat;
 }
 
-void part_verify(int* A, int p, int i, int j, int r) {
-
-    //for (unsigned g = 0; );
-
-    // byc moze wystarczy znalezc najwiekszy element pierwszej partycji
-    // i sprawdzic czy istnieje element mniejszy od tego najwiekszego w drugiej
-    // partycji.
-
-}
-
-void qsort(int* A, int p, int r)
-{
-    int buff;
-    if (p < r)
-    {
-        buff = partition(A, p, r);
-        qsort(A, p, buff);
-        qsort(A, buff + 1, r);
-
-    }
-
-}
-
-struct stats wstawienie(int* tab, unsigned int size) {
-
-    struct stats ret;
-
-    int* wynik = (int*)malloc(sizeof(int) * size);
-
-
-    for (unsigned int i = 0; i < size; i++)
-        wynik[i] = tab[i];
-
-
-
-    int buf, j;
-
-
-    for (unsigned int i = 1; i < size; i++) {
-        int buf = wynik[i];
+struct stats insertion_sort(int* tablica, int size) {
+    struct stats stat = {0, 0};
+    for (int i = 1; i < size; i++) {
+        int key = tablica[i];
         int j = i - 1;
-
-        // Przesuwanie elementów tablicy, aby zrobić miejsce na buf
-        while (j >= 0 && wynik[j] > buf) {
-            wynik[j + 1] = wynik[j];
-            --j;
-            ret.porownania++;
-            ret.wstawienia++;
-
+        while (j >= 0 && tablica[j] > key) {
+            tablica[j + 1] = tablica[j];
+            j--;
+            stat.porownania++;
+            stat.podstawienia++;
         }
-
-        wynik[j + 1] = buf;
-        ret.wstawienia++; // Wstawienie elementu
-
+        tablica[j + 1] = key;
+        stat.podstawienia++;
     }
-
-
-
-    printf("\n sortowanie przez wstawienie: porownania: %ld  wstawienia: %ld", ret.porownania, ret.wstawienia);
-
-#ifdef VERBOSE
-    for (unsigned int i = 0; i < size; i++)
-        printf("\n %d", wynik[i]);
-#endif
-
-    free(wynik);
-
-    return ret;
+    return stat;
 }
 
-struct stats bubble(int* tablica, unsigned int size) {
-
-    int* wynik = (int*)malloc(sizeof(int) * size);
-
-    struct stats ret;
-
-   
-    for (unsigned int i = 0; i < size; i++)
-        wynik[i] = tablica[i];
-
-   
-
-    int buf;
-
-    for(unsigned int i=0; i<size; i++) 
-        for (unsigned int j = 1; j < size - i; j++) {
-            ret.porownania++;
-            if (wynik[j - 1] > wynik[j]) {
-                buf = wynik[j - 1];
-                wynik[j - 1] = wynik[j];
-                wynik[j] = buf;
-                ret.wstawienia++;
+struct stats shell_sort(int* tablica, int size) {
+    struct stats stat = {0, 0};
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < size; i++) {
+            int temp = tablica[i];
+            int j;
+            for (j = i; j >= gap && tablica[j - gap] > temp; j -= gap) {
+                tablica[j] = tablica[j - gap];
+                stat.porownania++;
+                stat.podstawienia++;
             }
-
+            tablica[j] = temp;
+            stat.podstawienia++;
         }
+    }
+    return stat;
+}
 
+struct stats quick_sort(int* tablica, int size) {
+    struct stats stat = {0, 0};
+    quick_sort_recursive(tablica, 0, size - 1, &stat);
+    return stat;
+}
 
-    printf("\n babelkowe: porownania: %ld wstawienia: %ld", ret.porownania, ret.wstawienia);
+void quick_sort_recursive(int* A, int low, int high, struct stats* stat) {
+    if (low < high) {
+        int pi = partition(A, low, high, stat);
+        quick_sort_recursive(A, low, pi - 1, stat);
+        quick_sort_recursive(A, pi + 1, high, stat);
+    }
+}
 
-#ifdef VERBOSE
-    for (unsigned int i = 0; i < size; i++)
-        printf("\n %d", wynik[i]);
-#endif
+int partition(int* A, int low, int high, struct stats* stat) {
+    int pivot = A[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        stat->porownania++;
+        if (A[j] < pivot) {
+            i++;
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            stat->podstawienia++;
+        }
+    }
+    int temp = A[i + 1];
+    A[i + 1] = A[high];
+    A[high] = temp;
+    stat->podstawienia++;
+    return (i + 1);
+}
 
-    free(wynik);
+void copy_array(int* source, int* dest, int size) {
+    for (int i = 0; i < size; i++) {
+        dest[i] = source[i];
+    }
+}
 
-    return ret;
+struct stats aggregate_stats(struct stats* results, int count) {
+    struct stats summary = {LONG_MAX, LONG_MAX};
+    for (int i = 0; i < count; i++) {
+        summary.podstawienia = results[i].podstawienia;
+    }
+    return summary;
 }
